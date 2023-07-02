51c12a3c5d957de2487dc06d5b98effcc8f24934
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-07-02 15:57:24 +0400

    Implementing a simple controller that return a string “Hello World“

---
+++ src/main/java/com/DockerContainer/DockerContainer/DockerContainerApplication.java
@@ -0,0 +1,13 @@
+package com.DockerContainer.DockerContainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerContainerApplication.class, args);
+	}
+
+}
